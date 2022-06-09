22a0438520d0b5eefe71f2f787ac5418f973f538
AveSatanism <yanalfast@gmail.com>
2022-06-09 11:05:40 +0300

    remove Vue page

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,116 +1,42 @@
 <template>
-  <footer class="site_color_5_background">
-    <div
-      class="
-        footer_container
-        flex flex-wrap
-        justify-between
-        mx-auto
-      "
-    >
+  <footer class="w-full site_color_5_background">
+    <div class="footer_container flex flex-wrap justify-between mx-auto">
       <div class="pr-[10px]">
-        <a href="/"
-          ><img
-            class="mb-[32px]"
-            src="@/assets/logo/secondary.svg"
-            width="180"
-            height="50"
-            alt="Footer_logo"
-        /></a>
-        <p class="mb-[13px] text_6 site_color_1">
-          <a href="#">Политика обработки <br />персональных данных</a>
-        </p>
-        <p class="mb-[13px] text_6 site_color_1">
-          <a href="#">Политика конфиденциальности</a>
-        </p>
-        <h3 class="mt-[32px] site_color_4">
-          Перемены<br />
-          начинаются<br />
-          с нами!
-        </h3>
+        <a href="/"><img class="mb-[32px]" src="@/assets/logo/secondary.svg" width="180" height="50" alt="Footer_logo"/></a>
+        <p class="mb-[13px] text_6 site_color_1"><a href="#">Политика обработки <br />персональных данных</a></p>
+        <p class="mb-[13px] text_6 site_color_1"><a href="#">Политика конфиденциальности</a></p>
+        <h3 class="mt-[32px] site_color_4">Перемены<br /> начинаются<br /> с нами!</h3>
       </div>
       <div class="flex flex-wrap gap-x-[100px] py-[8px]">
         <ul class="text_6 site_color_1">
           <li class="mb-[13px] text_7"><p>меню</p></li>
-          <li class="mb-[13px]">
-            <p>
-              <a href="DigitalTransformation">цифровая <br />трансформация</a>
-            </p>
-          </li>
-          <li class="mb-[13px]">
-            <p>
-              <a href="InfrastructureManagement"
-                >управление <br />инфраструктурой</a
-              >
-            </p>
-          </li>
-          <li class="mb-[13px]">
-            <p><a href="AuditIT">аудит ит</a></p>
-          </li>
-          <li class="mb-[13px]">
-            <p>
-              <a href="#">ит-аутсорсинг <br />и техподдержка</a>
-            </p>
-          </li>
-          <li class="mb-[13px]">
-            <p><a href="#">разработка по</a></p>
-          </li>
-          <li class="mb-[13px]">
-            <p><a href="#">отраслевые решения</a></p>
-          </li>
+          <li class="mb-[13px]"><p><a href="DigitalTransformation">цифровая <br />трансформация</a></p></li>
+          <li class="mb-[13px]"><p><a href="InfrastructureManagement">управление <br />инфраструктурой</a></p></li>
+          <li class="mb-[13px]"><p><a href="AuditIT">аудит ит</a></p></li>
+          <li class="mb-[13px]"><p><a href="#">ит-аутсорсинг <br />и техподдержка</a></p></li>
+          <li class="mb-[13px]"><p><a href="#">разработка по</a></p></li>
+          <li class="mb-[13px]"><p><a href="#">отраслевые решения</a></p></li>
         </ul>
         <ul class="text_6 site_color_1">
           <li class="mb-[13px] text_7"><p>информация</p></li>
-          <li class="mb-[13px]">
-            <p><a href="#">для бизнеса</a></p>
-          </li>
-          <li class="mb-[13px]">
-            <p><a href="#">для партнёров</a></p>
-          </li>
-          <li class="mb-[13px]">
-            <p><a href="#">для соискателей</a></p>
-          </li>
+          <li class="mb-[13px]"><p><a href="#">для бизнеса</a></p></li>
+          <li class="mb-[13px]"><p><a href="#">для партнёров</a></p></li>
+          <li class="mb-[13px]"><p><a href="#">для соискателей</a></p></li>
         </ul>
         <div class="text_6 site_color_1">
           <p class="mb-[13px] text_7">контакты</p>
           <ul class="w-[167px] mb-[16px]">
-            <li class="flex justify-between normal-case">
-              <p>С-Пб</p>
-              <p class="site_color_2">8(812)-424-43-74</p>
-            </li>
-            <li class="flex justify-between normal-case">
-              <p>Москва</p>
-              <p class="site_color_2">8(495)-424-43-74</p>
-            </li>
+            <li class="flex justify-between normal-case"><p>С-Пб</p><p class="site_color_2">8(812)-424-43-74</p></li>
+            <li class="flex justify-between normal-case"><p>Москва</p><p class="site_color_2">8(495)-424-43-74</p></li>
           </ul>
           <p class="mb-[7px] text_7">Email</p>
           <p class="mb-[16px]">Help@smartics.ru</p>
           <div class="mb-[16px] flex gap-[12px]">
-            <img
-              src="@/assets/Skype.svg"
-              width="22.5"
-              height="23.33"
-              alt="skype_logo"
-            />
-            <img
-              src="@/assets/Watsapp.svg"
-              width="22.5"
-              height="23.33"
-              alt="watsapp_logo"
-            />
-            <img
-              src="@/assets/Telegram.svg"
-              width="23.33"
-              height="23.33"
-              alt="telegram_logo"
-            />
+            <img src="@/assets/Skype.svg" width="22.5" height="23.33" alt="skype_logo"/>
+            <img src="@/assets/Watsapp.svg" width="22.5" height="23.33" alt="watsapp_logo"/>
+            <img src="@/assets/Telegram.svg" width="23.33" height="23.33" alt="telegram_logo"/>
           </div>
-          <img
-            src="@/assets/qr-code.svg"
-            width="73"
-            height="73"
-            alt="qr-code"
-          />
+          <img src="@/assets/qr-code.svg" width="73" height="73" alt="qr-code"/>
         </div>
       </div>
     </div>
@@ -121,9 +47,6 @@
 </script>
 
 <style>
-footer {
-    width: 100%;
-}
 .footer_container {
   padding: 18px 30px;
   max-width: 1160px;
