614447263b3f38715a825a8c497ab91927834916
AveSatanism <yanalfast@gmail.com>
2022-07-22 16:05:07 +0300

    feat: add custom scrollbar for vacancies page

--- src/VuePage/VacanciesPage.vue
+++ src/VuePage/VacanciesPage.vue
@@ -6,7 +6,8 @@
       <h1 class="mb-[40px]">Наши вакансии</h1>
       <p class="mb-[44px]">Наши сотрудники - это члены большой семьи Smartics. Мы городимся каждым!<br> Присоединяйтесь к нашей команде!</p>
       <div class="flex justify-center">
-        <div class="p-[8px] vacancies_scroll h-[620px] flex flex-col gap-[40px] overflow-y-scroll">
+        <perfect-scrollbar class="h-[620px]">
+        <div class="pr-[8px] pl-[44px] vacancies_scroll h-[620px] flex flex-col gap-[40px]">
         <a href="Vacanci1"><UIVacanciesCard>
           <template #vacancies_title><p>стажёр по информационной безопасности</p></template>
           <template #description><p>SMARTICS — крупная российская ИТ-компания с сильной экспертизой в области информационной безопасности. Наша команда активно растёт, и мы ищем начинающих специалистов, которые хотят развивать практику SOC/SIEM и расти в этом направлении.</p></template>
@@ -43,6 +44,7 @@
           <template #employment><p>Полная занятость</p></template>
         </UIVacanciesCard>
         </div>
+        </perfect-scrollbar>
         <img class="vacancies_image h-[616px]" src="@/assets/pageContentImage/VacanciesPage_image.svg" width="344" height="616" alt="vacancies_image">
       </div>
     </div>
@@ -54,20 +56,29 @@ import SiteHeader from '../components/SiteHeader.vue'
 import SiteNavigation from '../components/SiteNavigation.vue'
 import UIVacanciesCard from '../components/UI/UIVacanciesCard.vue'
 import SiteFooter from '../components/SiteFooter.vue'
+import { PerfectScrollbar } from 'vue2-perfect-scrollbar'
 
 export default {
   components: {
     SiteHeader,
     SiteNavigation,
     UIVacanciesCard,
+    PerfectScrollbar,
     SiteFooter
     }
 }
 </script>
+<style src="vue2-perfect-scrollbar/dist/vue2-perfect-scrollbar.css"/>
 <style>
+.ps__thumb-y, .ps__rail-y:hover > .ps__thumb-y, .ps__rail-y:focus > .ps__thumb-y, .ps__rail-y.ps--clicking .ps__thumb-y {
+  background-color: var(--color-4);
+}
+
+.ps__rail-y {
+  left: 0px !important;
+}
 .vacancies_image {
   position: absolute;
   margin-left: 374px;
 }
-.vacancies_scroll::-webkit-scrollbar { width: 0 !important }
 </style>
