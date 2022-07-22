3137b29f16b4157763a32ff68e0963e4a9eb8bee
Andrey Yolkin <andreyyolkin@gmail.com>
2022-07-22 17:14:55 +0300

    wip: adds tabs component

---
+++ src/components/PriceCalculator.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="h-[400px]">
+    <div class="flex gap-2">
+      <button class="bg-[#44e]" @click="setCategory('cat1')">image.png</button>
+      <button class="bg-[#44e]" @click="setCategory('cat2')">1image.png</button>
+      <button class="bg-[#44e]" @click="setCategory('cat3')">3image.png</button>
+      <button class="bg-[#44e]" @click="setCategory('cat4')">4image.png</button>
+    </div>
+    <h2>category: {{ category }}</h2>
+    <h3 v-if="category === 'cat3'">I am cat 3</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      category: ''
+    }
+  },
+  methods: {
+    setCategory(categoryName) {
+      this.category = categoryName
+    }
+  }
+}
+</script>
+
+<style>
+</style>
