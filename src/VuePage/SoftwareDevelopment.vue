7728f4a7b39a731437d85d9f8baea1755db3a424
AveSatanism <yanalfast@gmail.com>
2022-08-11 20:07:15 +0300

    create SoftwareDevelopment page and slider for her

---
+++ src/VuePage/SoftwareDevelopment.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="SoftwareDevelopment">
+    <SiteHeader />
+    <SiteNavigation />
+    <div class="site_color_1 soft_dev_background">
+      <div class="flex flex-wrap mb-[87px] gap-x-[28.5px] justify-center">
+        <div class="mt-[51px] max-w-[552px] flex flex-col gap-[26px]">
+          <h1 class="text-h1">Разработка ПО</h1>
+          <h2 class="text-h2">Быстро.<br> Качественно.<br> С понятным ценообразованием.</h2>
+          <p class="text_1">Оказываем целый спектр услуг полного цикла
+            <br> разработки ПО: от вашей идеи до внедрения в
+            <br> рабочий процесс.
+            <br> Вам не нужно иметь технического образования,
+            <br> не нужно иметь специальных знаний,
+            <br> не нужно иметь опыт составления технических
+            <br> заданий.
+            <br><br>Просто опишите ваши идеи, а мы сделаем всё остальное!</p>
+          <UIButton variant="orange2">Хочу!</UIButton>
+        </div>
+        <img class="mt-[49px]" src="@/assets/pageContentImage/SoftwareDevelopment_image.svg" width="575" height="571" alt="SoftwareDevelopment_image">
+      </div>
+      <div>
+        <h3 class="mb-[28px] text-h3 text-center">МЫ ГОТОВЫ ОСУЩЕСТВИТЬ:</h3>
+      </div>
+      <div>
+        <h2 class="mb-[73px] text-h2 site_color_2 text-center">Примеры Наших решений:</h2>
+        <SofwareSlider />
+      </div>
+    </div>
+    <SiteFooter />
+  </div>
+</template>
+<script>
+import SiteHeader from '../components/SiteHeader.vue'
+import SiteNavigation from '../components/SiteNavigation.vue'
+import UIButton from '../components/UI/UIButton.vue'
+import SofwareSlider from '../components/SofwareSlider.vue'
+import SiteFooter from '../components/SiteFooter.vue'
+
+export default {
+  components: {
+    SiteHeader,
+    SiteNavigation,
+    UIButton,
+    SofwareSlider,
+    SiteFooter
+    }
+}
+</script>
+<style>
+.soft_dev_background {
+  background-image: url("@/assets/pageContentImage/SoftwareDevelopment_wave.svg");
+  background-size: 100%;
+  background-repeat: no-repeat;
+}
+</style>
---
+++ src/components/SofwareSlider.vue
@@ -0,0 +1,70 @@
+<template>
+<section class="software_slider_container flex justify-center mb-[67px]">
+<carousel-3d :controls-visible="true" :width="597" :height="398" :space="710" :inverseScaling="150" :perspective="0" :controlsWidth="27" :controlsHeight="27" :border="0" :count="3">
+  <slide :index="0">
+    <img src="@/assets/SoftwareDevelopmentSlider/Slide_image1.png" alt="Slide_image1">
+  </slide>
+  <slide :index="1">
+    <img src="@/assets/SoftwareDevelopmentSlider/Slide_image2.png" alt="Slide_image2">
+  </slide>
+  <slide :index="2">
+    <img src="@/assets/SoftwareDevelopmentSlider/Slide_image3.png" alt="Slide_image3">
+  </slide>
+</carousel-3d>
+</section>
+</template>
+
+<script>
+import { Carousel3d, Slide } from 'vue-carousel-3d';
+
+export default {
+  components: {
+    Carousel3d,
+    Slide
+  },
+}
+
+</script>
+<style>
+.software_slider_container .carousel-3d-container{
+  height: auto !important;
+  width: 900px !important;
+  overflow-x: hidden;
+  padding-bottom: 50px;
+  margin: 0 !important;
+}
+.software_slider_container .carousel-3d-slide{
+  border-radius: 10px;
+}
+.software_slider_container .right-1, .software_slider_container .left-1 {
+  filter: opacity(0.2);
+}
+.software_slider_container .carousel-3d-controls {
+  width: 95px !important;
+  margin: 0 !important;
+  top: 92% !important;
+  left: 44% !important;
+  z-index: 0 !important;
+}
+.software_slider_container .prev, .software_slider_container .next {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 6.5px;
+  background-color: var(--color-2);
+  border-radius: 50%;
+  font-size: 39px !important;
+  height: 27px;
+  width: 27px;
+}
+.software_slider_container .prev {
+  padding-right: 3px;
+}
+.software_slider_container .next {
+  padding-left: 3px;
+}
+.software_slider_container .prev span, .software_slider_container .next span {
+  color: white;
+}
+
+</style>
