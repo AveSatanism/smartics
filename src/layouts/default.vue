f4583962adf68a58a01f434a5c43638ecc064c80
Andrey Yolkin <andreyyolkin@gmail.com>
2022-08-12 12:45:40 +0300

    creates default layout with usual header and footer

---
+++ src/layouts/default.vue
@@ -0,0 +1,27 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <SiteHeader />
+    <SiteNavigation />
+    <slot />
+    <SiteFooter />
+  </div>
+</template>
+
+<script>
+import SiteHeader from '../components/SiteHeader.vue'
+import SiteNavigation from '../components/SiteNavigation.vue'
+import SiteFooter from '../components/SiteFooter.vue'
+
+export default {
+  components: {
+    SiteHeader,
+    SiteNavigation,
+    SiteFooter
+  }
+}
+</script>
+
+<style>
+
+</style>
