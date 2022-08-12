c8659b218715120d14981e3fba7f9665f8bccbc4
Andrey Yolkin <andreyyolkin@gmail.com>
2022-08-12 12:44:46 +0300

    feat: implements layout system

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <div id="app">
-    <router-view />
+    <component :is="layout">
+      <router-view/>
+    </component>
   </div>
 </template>
 
 <script>
+import EmptyLayout from '@/layouts/empty'
+
+export default {
+  computed: {
+    layout() {
+      return this.$route.meta.layout || EmptyLayout
+    }
+  }
+}
 </script>
 
 <style>
---
+++ src/layouts/empty.vue
@@ -0,0 +1,16 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
