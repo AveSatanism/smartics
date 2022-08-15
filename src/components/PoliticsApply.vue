7b5dd0bc90b78c10c8f77a4ced1d485dcf20fdfb
Andrey Yolkin <andreyyolkin@gmail.com>
2022-08-15 14:37:36 +0300

    feat: remember that cookie banner is hidden

--- src/components/PoliticsApply.vue
+++ src/components/PoliticsApply.vue
@@ -1,18 +1,27 @@
 <template>
-  <section class="politics_apply px-[30px] min-h-[86px] gap-x-[44px] flex justify-center items-center text-center text_7 site_color_3 site_color_1_background">
+  <section v-show="display"
+    class="politics_apply px-[30px] min-h-[86px] gap-x-[44px] flex justify-center items-center text-center text_7 site_color_3 site_color_1_background">
     <p>Продолжая использовать сайт smartics.ru вы соглашаетесь на использование файлов cookie.
-    <br>Подробности на странице <router-link to="PrivacyPolicy">политики конфиденциальности</router-link></p>
-    <UIButton variant="orange3">ХОРОШО</UIButton>   
-  </section>  
+      <br>Подробности на странице <router-link to="PrivacyPolicy">политики конфиденциальности</router-link>
+    </p>
+    <UIButton variant="orange3" @click="display = false">ХОРОШО</UIButton>
+  </section>
 </template>
 
 <script>
-  import UIButton from './UI/UIButton.vue'
+import UIButton from './UI/UIButton.vue'
 
-  export default {
-    components: {
-      UIButton
+import { useStorage } from '@vueuse/core'
+export default {
+  components: {
+    UIButton
+  },
+  setup() {
+    const display = useStorage('cookie', true)
+    return {
+      display
     }
+  }
 }
 </script>
 <style>
